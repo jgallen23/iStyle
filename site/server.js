3b6a86e2f4bf81ec094cfc7818ad7b141ae304f3
Greg Allen <me@jga.me>
2011-05-07 16:44:18 -0700

    refactored to use stylus, added site for example

---
+++ site/server.js
@@ -0,0 +1,44 @@
+var express = require("express");
+var app = express.createServer();
+var path = require("path");
+var fs = require("fs");
+
+var port = 3005;
+
+app.configure(function() {
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(app.router);
+
+	app.set("views", __dirname+"/templates");
+
+
+	var stylusCompile = function(str, path) {
+		console.log(arguments);
+		return stylus(str).set('filename', path).set('compress', true);
+	};
+
+	var stylus = require("stylus");
+	app.use(stylus.middleware({
+		src: path.join(__dirname, "../"),
+		dest: __dirname + '/public',
+		compile: stylusCompile
+	}));
+
+
+	app.use(express.static(__dirname+"/public"));
+
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+
+app.get("/", function(req, res) { 
+
+	locals = { 
+	};
+
+	res.render("index.jade", { layout: false, locals: locals });
+});
+
+app.listen(port, "0.0.0.0");
+console.log("Server started on port " + port);
